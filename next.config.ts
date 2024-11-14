c56af757eefb9bf056d00455d5e96c91e72c6932
Daniel Rahmi <50299064+rahmidaniel@users.noreply.github.com>
2024-11-14 21:06:06 +0100

    build fixes 0.7

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: '/peak-fe-stock-app',
 };
 
 export default nextConfig;
